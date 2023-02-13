8917b61b2c44d656a91c50e9f7cdc6e9eeab6b69
subbotin-n-o <subbotin.n.o@gmail.com>
2023-02-13 22:44:53 +0400

    #6 update UserGenerator.java

--- src/main/java/ru/yandex/practicum/diplom2/model/user/UserGenerator.java
+++ src/main/java/ru/yandex/practicum/diplom2/model/user/UserGenerator.java
@@ -6,14 +6,30 @@ import java.util.Locale;
 
 public class UserGenerator {
 
-    public static User getRandomUser() {
+    public static User getUser(UserType userType) {
         Faker faker = new Faker(new Locale("en"));
         User user = new User();
 
-        user.setEmail(faker.internet().emailAddress());
-        user.setPassword(faker.internet().password(6, 10));
-        user.setName(faker.name().firstName());
-
+        switch (userType) {
+            case VALID_USER:
+                user.setEmail(faker.internet().emailAddress());
+                user.setPassword(faker.internet().password(6, 10));
+                user.setName(faker.name().firstName());
+                break;
+            case NO_NAME_USER:
+                user.setEmail(faker.internet().emailAddress());
+                user.setPassword(faker.internet().password(6, 10));
+                break;
+            case NO_EMAIL_USER:
+                user.setPassword(faker.internet().password(6, 10));
+                user.setName(faker.name().firstName());
+                break;
+            case NO_PASSWORD_USER:
+                user.setEmail(faker.internet().emailAddress());
+                user.setName(faker.name().firstName());
+                break;
+        }
         return user;
     }
+
 }
