7aded7990d0f087f7b7ec1a13e3ef30873ac278c
subbotin-n-o <subbotin.n.o@gmail.com>
2023-02-13 23:58:11 +0400

    #13 update UserCredentials.java

--- src/main/java/ru/yandex/practicum/diplom2/model/user/UserCredentials.java
+++ src/main/java/ru/yandex/practicum/diplom2/model/user/UserCredentials.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.diplom2.model.user;
 
 import lombok.*;
 
+import static ru.yandex.practicum.diplom2.model.user.UserGenerator.getRandomEmail;
+import static ru.yandex.practicum.diplom2.model.user.UserGenerator.getRandomPassword;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,6 +15,16 @@ public class UserCredentials {
     private String password;
 
     public static UserCredentials from(User user) {
+        return new UserCredentials(user.getEmail(), user.getPassword());
+    }
+
+    public static UserCredentials notValidUserEmail(User user) {
+        user.setEmail(getRandomEmail());
+        return new UserCredentials(user.getEmail(), user.getPassword());
+    }
+
+    public static UserCredentials notValidUserPassword(User user) {
+        user.setPassword(getRandomPassword());
         return new UserCredentials(user.getEmail(), user.getPassword());
     }
 
