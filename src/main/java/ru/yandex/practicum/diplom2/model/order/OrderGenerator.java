3ac0193524096ad01b190255cb3e005bd91ce985
subbotin-n-o <subbotin.n.o@gmail.com>
2023-02-15 23:52:35 +0400

    #25 added OrderGenerator.java

---
+++ src/main/java/ru/yandex/practicum/diplom2/model/order/OrderGenerator.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.diplom2.model.order;
+
+
+import ru.yandex.practicum.diplom2.model.order.ingredients.Ingredients;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.yandex.practicum.diplom2.model.order.OrderClient.getIngredients;
+
+public class OrderGenerator {
+
+    private static final Order order = new Order();
+    private static final List<String> listIngredients = new ArrayList<>();
+
+    public static Order getListIngredients() {
+        Ingredients ingredients = getIngredients();
+
+        for (int i = 0; i < ingredients.getData().size(); i++) {
+            listIngredients.add(ingredients.getData().get(i).get_id());
+        }
+
+        order.setIngredients(listIngredients);
+        return order;
+    }
+}
